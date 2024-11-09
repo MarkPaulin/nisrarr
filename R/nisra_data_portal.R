0025b23bfe212ccfc631f07bc1e2b4fcf7b13bc0
Mark Paulin <paulin.mark@googlemail.com>
2024-11-09 13:31:33 +0000

    More tests and documentation

--- R/nisra_data_portal.R
+++ R/nisra_data_portal.R
@@ -53,11 +53,19 @@ nisra_data_portal <- function(method, ..., flush_cache = FALSE) {
 
 #' Read NISRA data portal dataset
 #'
+#' Fetch a dataset from the NISRA data portal using the dataset code. You
+#' can search for a dataset using [nisra_search()].
+#'
 #' @param dataset_code Dataset code
 #' @param flush_cache Ignore cached values
 #'
 #' @return A [tibble::tibble()] with the dataset.
 #' @export
+#'
+#' @example
+#' \dontrun{
+#'
+#' }
 nisra_read_dataset <- function(dataset_code, flush_cache = FALSE) {
   response <- nisra_data_portal(
     "PXStat.Data.Cube_API.ReadDataset",
@@ -148,11 +156,17 @@ nisra_read_collection <- function(datefrom = NULL, flush_cache = FALSE) {
 
 #' Search for a NISRA dataset
 #'
+#' Search the NISRA data portal for a dataset. You can search dataset titles
+#' either for a keyword or with a regular expression, using a dataset code, or
+#' by variables that appear in dataset. You can also specify how recently the
+#' dataset must have been updated.
+#'
 #' @param keyword Text to search for in dataset titles
 #' @param regex Regular expression for searching dataset titles
 #' @param dataset_code Dataset to find
 #' @param variables Variables to search for in datasets
-#' @param datefrom Date to search from
+#' @param datefrom Date to search from. Search is limited to datasets updated in 
+#' the last three months if not specified.
 #' @param flush_cache Ignore cached values
 #'
 #' @return A [tibble::tibble()] of dataset information matching the search terms
@@ -180,7 +194,12 @@ nisra_search <- function(keyword = NULL,
 
   if (!missing(variables)) {
     found <- vapply(coll[["dataset_dimensions"]], \(x) {
-      all(stringr::str_to_lower(variables) %in% stringr::str_to_lower(x))
+      all(vapply(variables, \(y) {
+        any(stringr::str_detect(
+          stringr::str_to_lower(x),
+          stringr::coll(y, ignore_case = TRUE)
+        ))
+      }, logical(1)))
     }, logical(1))
 
     coll <- coll[found, ]
--- tests/testthat/test-nisra_data_portal_request.R
+++ tests/testthat/test-nisra_data_portal_request.R
@@ -1,6 +1,9 @@
 test_that("nisra_data_portal_request uses parameters", {
   vcr::use_cassette("nisra_data_portal_request_uses_parameters", {
-    resp <- nisra_data_portal_request("test_method", params = list(made_up_parameter = "test"))
+    resp <- nisra_data_portal_request(
+      "test_method",
+      params = list(made_up_parameter = "test")
+    )
   })
 
   query_data <- httr2::resp_url_queries(resp)[["data"]] |>
@@ -28,14 +31,51 @@ vcr::use_cassette("nisra_data_portal_application_error", {
 
 
 vcr::use_cassette("nisra_search", {
-  test_that("nisra_search returns the results", {
-    out <- nisra_search("health", datefrom = "2024-10-14", flush_cache = TRUE, regex = "\\w")
+  test_that("nisra_search finds the right keywords", {
+    out <- nisra_search(
+      "health",
+      datefrom = "2024-10-14",
+      flush_cache = TRUE,
+      regex = "\\w"
+    )
+
     expect_equal(
       names(out),
-      c("dataset_code", "dataset_label", "frequency",
-        "dataset_dimensions", "updated")
+      c(
+        "dataset_code", "dataset_label", "frequency",
+        "dataset_dimensions", "updated"
+      )
     )
     labels <- out[["dataset_label"]]
     expect_true(all(grepl("health", labels, ignore.case = TRUE)))
+  })
+})
+
+vcr::use_cassette("nisra_search_2", {
+  test_that("nisra_search checks for dimensions", {
+    out <- nisra_search(
+      datefrom = "2024-10-14",
+      flush_cache = TRUE,
+      variables = "age"
+    )
+
+    data_dims <- out$dataset_dimensions
+    expect_true(
+      all(vapply(data_dims, \(x) {
+        any(stringr::str_detect(x, stringr::fixed("age", ignore_case = TRUE)))
+      }, logical(1)))
+    )
+  })
+})
+
+vcr::use_cassette("nisra_read_dataset", {
+  test_that("datasets are returned as tibbles", {
+    df <- nisra_read_dataset("CCMLGD", flush_cache = TRUE)
+
+    expect_s3_class(df, "tbl_df")
+    expect_identical(
+      colnames(df),
+      c("Statistic Label", "Month", "Local Government District", "UNIT", "VALUE")
+    )
   })
 })
